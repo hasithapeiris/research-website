dd21c603155167da08cc038df4a1bd66764ce447
Hasitha <hasithapeiris.me@gmail.com>
2025-05-24 10:28:32 +0530

    initial commit

---
+++ src/components/index.tsx
@@ -0,0 +1,17 @@
+import MainCarousel from "./MainCarousel";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SubHeader from "./SubHeader";
+import TeaProductionSummary from "./TeaProductionSummary";
+import FEESummary from "./FEESummary";
+import RegionalSummary from "./RegionalSummary";
+
+export {
+  MainCarousel,
+  Navbar,
+  Footer,
+  SubHeader,
+  TeaProductionSummary,
+  FEESummary,
+  RegionalSummary,
+};
